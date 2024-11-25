fc30b2b0f4d83d17f24b1f80e6c30b2570954328
Daniel Souza <workdndsza@gmail.com>
2024-11-25 17:38:39 -0300

    two way data binding e terminando o formulario

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!");
+  }
+
+  cancelar() {
+    alert('Cancelar');
+  }
 }
