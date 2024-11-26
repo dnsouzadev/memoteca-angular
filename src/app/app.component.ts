ee15ef337d0e66012a478f0cbce45c65f1418992
Daniel Souza <workdndsza@gmail.com>
2024-11-26 14:47:52 -0300

    criando o componente de listar os pensamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
 import { CriarPensamentoComponent } from "./components/pensamentos/criar-pensamento/criar-pensamento.component";
+import { ListarPensamentosComponent } from "./components/pensamentos/listar-pensamentos/listar-pensamentos.component";
 import { RodapeComponent } from "./components/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
