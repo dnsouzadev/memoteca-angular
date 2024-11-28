3ea75621e2c72d77e5579c091d7f042a06024a93
Daniel Souza <workdndsza@gmail.com>
2024-11-28 17:44:04 -0300

    implementando botao para carregar mais

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -4,21 +4,32 @@ import { RouterModule } from '@angular/router';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { PensamentoComponent } from "../pensamento/pensamento.component";
+import { BotaoCarregarMaisComponent } from "./botao-carregar-mais/botao-carregar-mais.component";
 
 @Component({
   selector: 'app-listar-pensamentos',
   standalone: true, // Adicione isto para marcar o componente como standalone
-  imports: [RouterModule, PensamentoComponent, HttpClientModule],
+  imports: [RouterModule, PensamentoComponent, HttpClientModule, BotaoCarregarMaisComponent],
   templateUrl: './listar-pensamentos.component.html',
   styleUrl: './listar-pensamentos.component.css'
 })
 export class ListarPensamentosComponent {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe(pensamentos => this.listaPensamentos = pensamentos);
+  }
+
+  carregarMaisPensamentos(): void {
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
