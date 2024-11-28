2db4c2fc27652756664bf538dfa8c84ad2bff9fd
Daniel Souza <workdndsza@gmail.com>
2024-11-28 18:03:59 -0300

    criando um campo de busca

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
@@ -9,7 +10,7 @@ import { BotaoCarregarMaisComponent } from "./botao-carregar-mais/botao-carregar
 @Component({
   selector: 'app-listar-pensamentos',
   standalone: true, // Adicione isto para marcar o componente como standalone
-  imports: [RouterModule, PensamentoComponent, HttpClientModule, BotaoCarregarMaisComponent],
+  imports: [RouterModule, PensamentoComponent, HttpClientModule, BotaoCarregarMaisComponent, FormsModule],
   templateUrl: './listar-pensamentos.component.html',
   styleUrl: './listar-pensamentos.component.css'
 })
@@ -17,19 +18,26 @@ export class ListarPensamentosComponent {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
+  filtro: string = '';
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe(pensamentos => this.listaPensamentos = pensamentos);
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(pensamentos => this.listaPensamentos = pensamentos);
   }
 
   carregarMaisPensamentos(): void {
-    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length) {
         this.haMaisPensamentos = false;
       }
     });
+  }
+
+  pesquisarPensamentos(): void {
+    this.paginaAtual = 1;
+    this.haMaisPensamentos = true;
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(pensamentos => this.listaPensamentos = pensamentos);
   }
 }
